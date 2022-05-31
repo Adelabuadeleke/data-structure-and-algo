8ecef53a5d2b5b96f193ff6f7e47ecfbe2743f8c
Adeleke <kamade123@gmail.com>
2022-05-31 20:35:25 +0100

    hashtable: remove functionality edited, lookup added

--- 6-hash_tables.js
+++ 6-hash_tables.js
@@ -40,12 +40,26 @@ let HashTable = function() {
     if(storage[index].length === 1 && storage[index][0][0] === key) {
       delete storage[index];
     } else {
-      for (var i = 0; i < storage[index]; i++) {
+      for (var i = 0; i < storage[index].length; i++) {
         if(storage[index][i][0] === key) {
           delete storage[index][i];
         }
       }
     }
   };
-  
+
+  this.lookup = function(key) {
+    var index = hash(key, storageLimit);
+    if(storage[index] = undefined) {
+      return undefined;
+    } else {
+      for (var i = 0; i < storage[index].length; i++) {
+        if(storage[index][i][0] === key){
+          return storage[index][i][1];
+        }
+      }
+    }
+  }
 }
+
+console.log(hash('quincy', 10));
