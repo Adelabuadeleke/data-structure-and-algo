7c27fd541d9f316d58ae745d7bd64ea35eb0b5a1
Adeleke <kamade123@gmail.com>
2022-06-01 20:42:17 +0100

    linked list add funmctionality added

--- 7-linked_list.js
+++ 7-linked_list.js
@@ -15,4 +15,19 @@ function LinkedList(){
   this.head = function() {
     return head;
   }
+
+  this.add = function(element) {
+    var node = new Node(element);
+    if(head === null) {
+      head = node;
+    } else {
+      var currentNode = head;
+      while(currentNode.next){
+        currentNode = currentNode.next;
+      }
+      length++;
+    };
+
+    
+  }
 }
