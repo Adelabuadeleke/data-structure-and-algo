c22f147feaf1ffb9a7d8ff183bbbe72e59fb6c93
Adeleke <kamade123@gmail.com>
2022-06-02 22:31:50 +0100

    trie node setup added

--- 8-trie.js
+++ 8-trie.js
@@ -1,1 +1,12 @@
-/* Trie*/ 
+/* Trie Data Sructure*/ 
+
+let Node = function() {
+  this.keys = new Map();
+  this.end = false;
+  this.setEnd = function() {
+    return this.end = true;
+  };
+  this.isEnd = function() {
+    return this.end;
+  };
+};
